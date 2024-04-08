aeb70222b603ccc5e42be87e2cf35c9e0e7766d6
KhrystynaStasiv <khrystynalysanyk@gmail.com>
2024-04-08 22:27:51 +0300

    final changes

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,10 @@
 function getElementWidth(content, padding, border) {
-    return content + 2 * padding + 2 * border;
+  content = Number.parseFloat(content);
+  padding = Number.parseFloat(padding);
+  border = Number.parseFloat(border);
+  const boxSizing = content + (padding + padding) + (border + border);
+  return boxSizing;
 }
-getElementWidth(content, padding, border)
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(getElementWidth('50px', '8px', '4px')); // 74
+console.log(getElementWidth('60px', '12px', '8.5px')); // 101
+console.log(getElementWidth('200px', '0px', '0px')); // 200
